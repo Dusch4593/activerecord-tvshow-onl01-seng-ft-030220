35452e4209da651139e4e8032a878b60095fce21
Brandon Dusch <brandondusch@gmail.com>
2020-04-09 19:10:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,20 @@
 class Show < ActiveRecord::Base 
   def highest_rating 
+    Show.maximum(:)
   end
   
   def most_popular_show 
   end
+  
+  def least_popular_show
+  end 
+  
+  def ratings_sum 
+  end 
+  
+  def popular_shows 
+  end 
+  
+  def shows_by_alphabetical_order 
+  end 
 end
